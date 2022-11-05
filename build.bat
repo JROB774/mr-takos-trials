13e5edc2aa8053f7c5fef03abb5abc9ade2453f7
Joshua Robertson <joshrobertson774@gmail.com>
2022-11-05 02:27:26 +0000

    Update build script to support compiling both Windows and Web builds.

--- build.bat
+++ build.bat
@@ -1,10 +1,14 @@
 @echo off
 setlocal
 
-if not exist binary\win32 mkdir binary\win32
+if "%~1"=="win32" goto build_win32
+if "%~1"=="web" goto build_web
 
-copy depends\sdl_mixer\bin\win32\*.dll binary\win32\ > NUL
-copy depends\sdl\bin\win32\*.dll binary\win32\ > NUL
+echo please specify a platform to build for (win32 or web)...
+goto end
+
+:build_win32
+echo ----------------------------------------
 
 set defs=-D BUILD_DEBUG -D SDL_MAIN_HANDLED
 set idir=-I ../../depends/sdl/include -I ../../depends/sdl_mixer/include
@@ -13,8 +17,44 @@ set libs=SDL2main.lib SDL2.lib SDL2_mixer.lib
 set cflg=
 set lflg=-incremental:no
 
+if "%~2"=="release" (
+    set defs=
+    set cflg=%cflg% -O2
+    set lflg=%lflg% -release -subsystem:windows
+)
+
+if not exist binary\win32 mkdir binary\win32
+
+copy depends\sdl_mixer\bin\*.dll binary\win32\ > NUL
+copy depends\sdl\bin\*.dll binary\win32\ > NUL
+
 pushd binary\win32
-cl ../../source/main.c %cflg% %defs% %idir% -Fe:tako.exe -link %lflg% %ldir% %libs%
+cl ../../source/main_win32.c %cflg% %defs% %idir% -Fe:tako.exe -link %lflg% %ldir% %libs%
 popd
+
+goto end
+
+:build_web
+echo ----------------------------------------
+
+call depends\emsdk\emsdk install latest
+call depends\emsdk\emsdk activate latest
+call depends\emsdk\emsdk_env.bat
+
+set defs=
+set idir=
+set libs=-s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_OGG=1 -s USE_VORBIS=1 -lidbfs.js
+set cflg=
+set lflg=-s EXPORTED_FUNCTIONS="['_main']" -s EXPORTED_RUNTIME_METHODS="['ccall']"
+
+if not exist binary\web mkdir binary\web
+
+pushd binary\web
+emcc %libs% %idir% %cflg% %lflg% ../../source/main_web.c -o tako.html
+popd
+
+goto end
+
+:end
 
 endlocal
