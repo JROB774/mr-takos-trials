4d784cd1ac97af9dbc0dc9ec9fb65085b5464a80
Joshua Robertson <joshrobertson774@gmail.com>
2022-11-06 09:18:19 +0000

    Merge platform files + get web build updated to match native build.

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ goto end
 :build_win32
 echo ----------------------------------------
 
-set defs=-D BUILD_DEBUG -D SDL_MAIN_HANDLED
+set defs=-D BUILD_NATIVE
 set idir=-I ../../depends/sdl/include -I ../../depends/sdl_mixer/include -I ../../depends/nksdk/nklibs
 set ldir=-libpath:../../depends/sdl/lib -libpath:../../depends/sdl_mixer/lib
 set libs=SDL2main.lib SDL2.lib SDL2_mixer.lib opengl32.lib shell32.lib
@@ -18,10 +18,14 @@ set cflg=
 set lflg=-incremental:no
 
 if "%~2"=="release" (
-    set defs=
+
     set cflg=%cflg% -O2
     set lflg=%lflg% -release -subsystem:windows
+) else (
+    set defs=%defs% -D BUILD_DEBUG -D SDL_MAIN_HANDLED
 )
+
+set defs=%defs% -D BUILD_NATIVE
 
 if not exist binary\win32 mkdir binary\win32
 
@@ -29,7 +33,7 @@ copy depends\sdl_mixer\bin\*.dll binary\win32\ > NUL
 copy depends\sdl\bin\*.dll binary\win32\ > NUL
 
 pushd binary\win32
-cl ../../source/platform_win32.c %cflg% %defs% %idir% -Fe:tako.exe -link %lflg% %ldir% %libs%
+cl ../../source/platform.c %cflg% %defs% %idir% -Fe:tako.exe -link %lflg% %ldir% %libs%
 popd
 
 goto end
@@ -41,16 +45,16 @@ call depends\emsdk\emsdk install latest
 call depends\emsdk\emsdk activate latest
 call depends\emsdk\emsdk_env.bat
 
-set defs=
-set idir=
+set defs=-D BUILD_WEB
+set idir=-I ../../depends/nksdk/nklibs
 set libs=-s WASM=1 -s USE_SDL=2 -s USE_SDL_MIXER=2 -s USE_OGG=1 -s USE_VORBIS=1 -lidbfs.js
 set cflg=
-set lflg=-s EXPORTED_FUNCTIONS="['_main']" -s EXPORTED_RUNTIME_METHODS="['ccall']"
+set lflg=-s EXPORTED_FUNCTIONS="['_main', '_main_callback']" -s EXPORTED_RUNTIME_METHODS="['ccall']"
 
 if not exist binary\web mkdir binary\web
 
 pushd binary\web
-emcc %libs% %idir% %cflg% %lflg% ../../source/platform_web.c -o tako.html
+emcc %libs% %idir% %cflg% %lflg% %defs% ../../source/platform.c -o tako.html
 popd
 
 goto end
