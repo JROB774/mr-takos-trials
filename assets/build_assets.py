a5e42fd888b17ea87d8355cdd05f2745c1e4da31
Joshua Robertson <joshrobertson774@gmail.com>
2022-11-09 21:52:17 +0000

    Setup basic texture atlas packing process.

--- assets/build_assets.py
+++ assets/build_assets.py
@@ -2,6 +2,7 @@ import sys
 import os
 import shutil
 
+from rectpack import newPacker
 from PIL import Image
 
 if len(sys.argv) <= 1:
@@ -17,23 +18,7 @@ asset_dir = "assets/"
 if os.path.exists(asset_out_dir):
     shutil.rmtree(asset_out_dir)
 
-# build textures
-texture_out_dir = asset_out_dir + "textures/"
-texture_in_dir = asset_dir + "textures/"
-
-os.makedirs(texture_out_dir)
-
-texture_files = os.listdir(path=texture_in_dir)
-for file in texture_files:
-    infile = os.path.join(texture_in_dir, file)
-    if os.path.isfile(infile):
-        if infile.split(".")[1] == "psd":
-            outfile = str.replace(os.path.join(texture_out_dir, file), ".psd", ".png")
-            print("converting texture {} to {}".format(infile, outfile))
-            img = Image.open(infile)
-            img.save(outfile)
-
-# build shaders
+# build platform shaders
 shader_out_dir = asset_out_dir + "shaders/"
 shader_in_dir = asset_dir + "shaders/"
 
@@ -57,3 +42,73 @@ for file in shader_files:
                     src = "precision mediump float;\n\n" + src
                     with open(outfile, "w") as outshader:
                         outshader.write(src)
+
+# crop temp textures
+texture_out_dir = asset_out_dir + "textures/"
+texture_in_dir = asset_dir + "textures/"
+texture_tmp_dir = texture_in_dir + "temp/"
+
+if os.path.exists(texture_tmp_dir):
+    shutil.rmtree(texture_tmp_dir)
+
+os.makedirs(texture_out_dir)
+os.makedirs(texture_tmp_dir)
+
+atlas_lists = {}
+
+atlas_padding = 8
+
+texture_files = os.listdir(path=texture_in_dir)
+for file in texture_files:
+    infile = os.path.join(texture_in_dir, file)
+    if os.path.isfile(infile):
+        if infile.split(".")[1] == "psd":
+            # crop each input image and save it as a temporary png for atlas packing
+            tmpfile = str.replace(os.path.join(texture_tmp_dir, file), ".psd", ".png")
+            print("cropping texture {} to {}".format(infile, tmpfile))
+            img = Image.open(infile)
+            img = img.crop(img.getbbox())
+            img.save(tmpfile)
+            atlas_name = os.path.basename(tmpfile).split('_')[0].split('.')[0]
+            if atlas_name not in atlas_lists:
+                atlas_lists[atlas_name] = []
+            atlas_lists[atlas_name].append(tmpfile)
+
+# build texture atlases
+for name,files in atlas_lists.items():
+    if len(files) > 1:
+        outfile = os.path.join(texture_out_dir, name + ".png")
+        print("building texture atlas {}".format(outfile))
+        # load all of the textures
+        textures = []
+        rects = []
+        for file in files:
+            img = Image.open(file)
+            textures.append(img)
+            w = img.size[0] + atlas_padding
+            h = img.size[1] + atlas_padding
+            rects.append((w,h))
+        # pack the texture rects
+        for atlas_pow in range(1,12):
+            packer = newPacker()
+            for rect in rects:
+                packer.add_rect(*rect)
+            atlas_size = pow(2, atlas_pow)
+            packer.add_bin(atlas_size,atlas_size)
+            packer.pack()
+            if len(packer) and len(packer[0]) == len(rects): # increas size until successful
+                break
+        # build the final atlas image
+        atlas = Image.new("RGBA", (atlas_size,atlas_size))
+        for i in range(len(textures)):
+            x = int(packer[0][i].x + (atlas_padding / 2))
+            y = int(packer[0][i].y + (atlas_padding / 2))
+            atlas.paste(textures[i], (x,y))
+        atlas.save(outfile)
+    else:
+        # save out the original
+        infile = str.replace(os.path.join(texture_in_dir, os.path.basename(files[0])), ".png", ".psd")
+        outfile = str.replace(os.path.join(texture_out_dir, os.path.basename(files[0])), ".psd", ".png")
+        print("converting texture {} texture to {}".format(infile, outfile))
+        img = Image.open(infile)
+        img.save(outfile)
