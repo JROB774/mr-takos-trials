17e953b50f09e06fb2bbecc26c7ead273efd3538
Joshua Robertson <joshrobertson774@gmail.com>
2022-11-16 23:43:57 +0000

    Start stubbing out the other menus.

--- source/menu.c
+++ source/menu.c
@@ -1,5 +1,15 @@
 /*////////////////////////////////////////////////////////////////////////////*/
 
+NK_ENUM(MenuState, nkS32)
+{
+    MenuState_Title,
+    MenuState_Main,
+    MenuState_Options,
+    MenuState_Awards,
+    MenuState_Credits,
+    MenuState_TOTAL
+};
+
 NK_ENUM(MainMenuOption, nkS32)
 {
     MainMenuOption_Play,
@@ -10,20 +20,15 @@ NK_ENUM(MainMenuOption, nkS32)
     MainMenuOption_TOTAL
 };
 
-static const nkChar* MAIN_MENU_OPTIONS[] =
-{
-    "Play Game",
-    "Options",
-    "Awards",
-    "Credits",
-    "Exit"
-};
+static const nkChar* MAIN_MENU_OPTIONS[] = { "Play Game", "Options", "Awards", "Credits", "Exit" };
 
 NK_STATIC_ASSERT(MainMenuOption_TOTAL == NK_ARRAY_SIZE(MAIN_MENU_OPTIONS), main_menu_option_size_mismatch);
 
+static MenuState g_menustate;
+
 static void menu_init(void)
 {
-    // Nothing...
+    g_menustate = MenuState_Title;
 }
 
 static void menu_quit(void)
@@ -31,7 +36,15 @@ static void menu_quit(void)
     // Nothing...
 }
 
-static void menu_update(nkF32 dt)
+static void menu_update_title(nkF32 dt)
+{
+    if(is_mouse_button_pressed(MouseButton_Left))
+    {
+        g_menustate = MenuState_Main;
+    }
+}
+
+static void menu_update_main(nkF32 dt)
 {
     nkF32 h = font_get_px_height(g_asset_font) * 0.75f;
     nkF32 y = ((SCREEN_HEIGHT - ((MainMenuOption_TOTAL-1) * h)) * 0.5f) + (h * 0.25f);
@@ -61,7 +74,39 @@ static void menu_update(nkF32 dt)
     }
 }
 
-static void menu_render(void)
+static void menu_update_options(nkF32 dt)
+{
+    // @Incomplete: ...
+}
+
+static void menu_update_awards(nkF32 dt)
+{
+    // @Incomplete: ...
+}
+
+static void menu_update_credits(nkF32 dt)
+{
+    // @Incomplete: ...
+}
+
+static void menu_update(nkF32 dt)
+{
+    switch(g_menustate)
+    {
+        case MenuState_Title: menu_update_title(dt); break;
+        case MenuState_Main: menu_update_main(dt); break;
+        case MenuState_Options: menu_update_options(dt); break;
+        case MenuState_Awards: menu_update_awards(dt); break;
+        case MenuState_Credits: menu_update_credits(dt); break;
+    }
+}
+
+static void menu_render_title(void)
+{
+    // @Incomplete: ...
+}
+
+static void menu_render_main(void)
 {
     nkF32 h = font_get_px_height(g_asset_font) * 0.75f;
     nkF32 y = ((SCREEN_HEIGHT - ((MainMenuOption_TOTAL-1) * h)) * 0.5f) + (h * 0.25f);
@@ -85,6 +130,33 @@ static void menu_render(void)
         font_draw_text(g_asset_font, x,y, MAIN_MENU_OPTIONS[i], fg_color);
 
         y += h;
+    }
+}
+
+static void menu_render_options(void)
+{
+    // @Incomplete: ...
+}
+
+static void menu_render_awards(void)
+{
+    // @Incomplete: ...
+}
+
+static void menu_render_credits(void)
+{
+    // @Incomplete: ...
+}
+
+static void menu_render(void)
+{
+    switch(g_menustate)
+    {
+        case MenuState_Title: menu_render_title(); break;
+        case MenuState_Main: menu_render_main(); break;
+        case MenuState_Options: menu_render_options(); break;
+        case MenuState_Awards: menu_render_awards(); break;
+        case MenuState_Credits: menu_render_credits(); break;
     }
 }
 
